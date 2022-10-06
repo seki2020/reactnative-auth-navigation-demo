ea3ae0f3ad869bdb001ec92680c75005682bc17c
xm <v8main@gmail.com>
2022-10-06 14:48:10 +0800

    add back to modal

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Pressable } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { RootStackScreenProps } from '../types';
 
-export default function ModalScreen() {
+export default function ModalScreen({ navigation }: RootStackScreenProps<'Modal'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Pressable
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Text style={styles.title}>Click to close</Text>
+      </Pressable>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ModalScreen.tsx" />
 
